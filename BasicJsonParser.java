cac5cc15eb961a5b3d9e12e242711e76a4f64b28
Eren Aydın <101468578+paminks@users.noreply.github.com>
2023-10-14 11:23:49 +0300

    Update BasicJsonParser.java

--- BasicJsonParser.java
+++ BasicJsonParser.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 public class BasicJsonParser {
 
     public static void main(String[] args) {
+        //change targetDirectory with your desired directory, I believe this can be static too depending on your case of use 
         String targetDirectory = "/your/directory/here";
         String fileExtension = ".json";
+        //change this to your desired keyword
         String searchString = "path";
 
         File directory = new File(targetDirectory);
@@ -18,6 +20,7 @@ public class BasicJsonParser {
                 if (file.isFile() && file.getName().endsWith(fileExtension)) {
                     String result = searchFileForStringAndExtract(file, searchString);
                     if (result != null) {
+                        //this replaces the "" with a null string so you read the actual data without ""
                         result = result.replace("\"", "");
                         System.out.println("String found in: " + file.getName() + ", Extracted: " + result);
                     
@@ -32,6 +35,7 @@ public class BasicJsonParser {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.contains(searchString) && line.contains(":")) {
+                    //this removes : from the json file so you can read the actual data
                     int colonIndex = line.indexOf(":");
                     int startIndex = line.indexOf(searchString);
                     if (startIndex < colonIndex) {
